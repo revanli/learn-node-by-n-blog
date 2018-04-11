b016504f6d8fa4cf8c64fad3b978cbaca34289aa
revanli <justin_yufan@163.com>
2018-04-11 23:43:47 +0800

    view engine

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const indexRouter = require('./routes/index')
 const userRouter = require('./routes/users')
+
+app.set('views', path.join(__dirname, 'views'))  // 模板文件的存放目录
+app.set('view engine', 'ejs')
 
 app.use('/', indexRouter)
 app.use('/users', userRouter)
