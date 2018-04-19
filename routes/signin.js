5df071af9eb0360bd8eecfc0278bd265bc8b6a62
revanli <justin_yufan@163.com>
2018-04-19 09:44:48 +0800

    add routes

---
+++ routes/signin.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+console.log('res>>>', require('../middlewares/check'))
+
+// const checkNotLogin = require('../middlewares/check').checkNotLogin
+
+// GET /signin 登录页
+// router.get('/', checkNotLogin, function (req, res, next) {
+//   res.send('登录页')
+// })
+
+// // POST /signin 用户登录
+// router.post('/', checkNotLogin, function (req, res, next) {
+//   res.send('登录')
+// })
+
+module.exports = router
